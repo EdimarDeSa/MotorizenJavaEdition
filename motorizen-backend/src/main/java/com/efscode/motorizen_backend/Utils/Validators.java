2292df788e809cdcf858e1ec847df990660699b4
EdimarDeSa <edimar.sa@efscode.com.br>
2025-07-15 03:52:31 +0000

    Separação de classe para validação de dados/regras de negócios

---
+++ motorizen-backend/src/main/java/com/efscode/motorizen_backend/Utils/Validators.java
@@ -0,0 +1,65 @@
+package com.efscode.motorizen_backend.Utils;
+
+import org.springframework.stereotype.Component;
+
+import com.efscode.motorizen_backend.enums.MotoriZenResponseCodeEnum;
+import com.efscode.motorizen_backend.errors.MotorizenException;
+import com.efscode.motorizen_backend.models.dtos.BrandDTO;
+import com.efscode.motorizen_backend.models.dtos.NewUser;
+import com.efscode.motorizen_backend.models.dtos.UserDTO;
+import com.efscode.motorizen_backend.repositorys.BackLogRepository;
+import com.efscode.motorizen_backend.repositorys.BrandRepository;
+import com.efscode.motorizen_backend.repositorys.FuelTypeRepository;
+import com.efscode.motorizen_backend.repositorys.RegisterRepository;
+import com.efscode.motorizen_backend.repositorys.UserRepository;
+import com.efscode.motorizen_backend.repositorys.VehicleRepository;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Component
+@SuppressWarnings("unused")
+public class Validators {
+  private final UserRepository userRepo;
+  private final BrandRepository brandRepo;
+  private final VehicleRepository vehicleRepo;
+  private final FuelTypeRepository fuelTypeRepo;
+  private final RegisterRepository registerRepo;
+  private final BackLogRepository backLogRepo;
+
+  public void validateBrand(BrandDTO brand) {
+    validateLength(brand.name(), 2, 50, MotoriZenResponseCodeEnum.INVALID_BRAND_NAME);
+  }
+
+  public void validateUser(UserDTO user) {
+    validateLength(user.firstName(), 2, 50, MotoriZenResponseCodeEnum.INVALID_USER_NAME);
+    validateLength(user.lastName(), 2, 100, MotoriZenResponseCodeEnum.INVALID_USER_NAME);
+    validateLength(user.email(), 2, 255, MotoriZenResponseCodeEnum.INVALID_USER_EMAIL);
+
+    if (user.getAge() < 18) {
+      throw new MotorizenException(MotoriZenResponseCodeEnum.USER_MUST_BE_18);
+    }
+
+    if (!user.email().matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
+      throw new MotorizenException(MotoriZenResponseCodeEnum.INVALID_USER_EMAIL);
+    }
+  }
+
+  public void validateNewUser(NewUser newUser) {
+    this.validateUser(newUser.toUserDTO());
+    if (userRepo.existsByEmail(newUser.getEmail().toLowerCase())) {
+      throw new MotorizenException(MotoriZenResponseCodeEnum.USER_ALREADY_EXISTS);
+    }
+
+    if (!newUser.getPassword().matches(
+        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$")) {
+      throw new MotorizenException(MotoriZenResponseCodeEnum.INVALID_PASSWORD);
+    }
+  }
+
+  private void validateLength(String toValidate, int minLength, int maxLength, MotoriZenResponseCodeEnum rc) {
+    if (toValidate.length() < minLength || toValidate.length() > maxLength) {
+      throw new MotorizenException(rc);
+    }
+  }
+}
