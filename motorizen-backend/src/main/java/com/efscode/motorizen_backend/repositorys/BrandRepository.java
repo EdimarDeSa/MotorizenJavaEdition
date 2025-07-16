c2b79908891a0318732adc0baa3a7e049b835e29
EdimarDeSa <edimar.sa@efscode.com.br>
2025-07-16 13:51:34 +0000

    Simplificando query

--- motorizen-backend/src/main/java/com/efscode/motorizen_backend/repositorys/BrandRepository.java
+++ motorizen-backend/src/main/java/com/efscode/motorizen_backend/repositorys/BrandRepository.java
@@ -3,15 +3,13 @@ package com.efscode.motorizen_backend.repositorys;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.efscode.motorizen_backend.models.entitys.BrandEntity;
 
 @Repository
 public interface BrandRepository extends JpaRepository<BrandEntity, Integer> {
-  @Query("SELECT b FROM BrandEntity b WHERE b.deletedAt IS NULL")
-  List<BrandEntity> findAllAndNotDeleted();
+  List<BrandEntity> findByDeletedAtIsNull();
 
   List<BrandEntity> findByNameContainingIgnoreCase(String name);
 
