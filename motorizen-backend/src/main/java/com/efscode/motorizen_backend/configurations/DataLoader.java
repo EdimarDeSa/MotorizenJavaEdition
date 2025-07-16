f7fb36d293038f6be323cbd6c70a0c3e9913dd86
EdimarDeSa <edimar.sa@efscode.com.br>
2025-07-16 04:06:44 +0000

    Configurado dados default de forma que o hibernate não cague tudo

--- motorizen-backend/src/main/java/com/efscode/motorizen_backend/configurations/DataLoader.java
+++ motorizen-backend/src/main/java/com/efscode/motorizen_backend/configurations/DataLoader.java
@@ -1,5 +1,6 @@
 package com.efscode.motorizen_backend.configurations;
 
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -7,10 +8,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.efscode.motorizen_backend.models.dtos.NewBrandDTO;
+import com.efscode.motorizen_backend.models.dtos.NewFuelType;
 import com.efscode.motorizen_backend.models.dtos.NewUser;
 import com.efscode.motorizen_backend.models.entitys.UserEntity;
+import com.efscode.motorizen_backend.repositorys.BrandRepository;
+import com.efscode.motorizen_backend.repositorys.FuelTypeRepository;
 import com.efscode.motorizen_backend.repositorys.UserRepository;
+import com.efscode.motorizen_backend.services.BrandService;
+import com.efscode.motorizen_backend.services.FuelTypeService;
 import com.efscode.motorizen_backend.services.UserService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +31,12 @@ public class DataLoader implements CommandLineRunner {
   private final UserRepository userRepo;
   private final UserService userService;
 
+  private final BrandRepository brandRepo;
+  private final BrandService brandService;
+
+  private final FuelTypeRepository fuelTypeRepo;
+  private final FuelTypeService fuelTypeService;
+
   @Value("${spring.profiles.active}")
   private String mode;
 
@@ -31,6 +46,58 @@ public class DataLoader implements CommandLineRunner {
 
     if ("dev".equals(mode))
       loadUsers();
+
+    loadBrands();
+
+    loadFuelTypes();
+
+    log.info("Base de dados carregada com sucesso!");
+  }
+
+  private void loadFuelTypes() {
+    if (fuelTypeRepo.count() != 0)
+      return;
+
+    log.info("Iniciando tipos de combustível...");
+
+    ObjectMapper mapper = new ObjectMapper();
+    TypeReference<List<String>> typeReference = new TypeReference<>() {
+    };
+    InputStream inputStream = TypeReference.class.getResourceAsStream("/defaults/fuel_types.json");
+
+    try {
+      List<String> fuelTypes = mapper.readValue(inputStream, typeReference);
+      fuelTypes.forEach(fuelType -> {
+        NewFuelType newFuelType = new NewFuelType(fuelType);
+        fuelTypeService.createNewFuelType(newFuelType);
+      });
+      log.info("Tipos de combustível carregados com sucesso!");
+    } catch (Exception e) {
+      log.error("Erro ao carregar tipos de combustível", e);
+    }
+  }
+
+  private void loadBrands() {
+    if (brandRepo.count() != 0)
+      return;
+
+    log.info("Iniciando marcas...");
+
+    ObjectMapper mapper = new ObjectMapper();
+    TypeReference<List<String>> typeReference = new TypeReference<>() {
+    };
+    InputStream inputStream = TypeReference.class.getResourceAsStream("/defaults/brands.json");
+
+    try {
+      List<String> brands = mapper.readValue(inputStream, typeReference);
+      brands.forEach(brand -> {
+        NewBrandDTO newBrand = new NewBrandDTO(brand);
+        brandService.createNewBrand(newBrand);
+      });
+      log.info("Marcas carregadas com sucesso!");
+    } catch (Exception e) {
+      log.error("Erro ao carregar marcas", e);
+    }
   }
 
   private void loadUsers() {
@@ -63,4 +130,5 @@ public class DataLoader implements CommandLineRunner {
       }
     });
   }
+
 }
